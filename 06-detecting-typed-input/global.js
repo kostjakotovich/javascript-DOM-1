68c72d4e381c7936a98da8ec52c182be9cdb1b60
kostjakotovich <kostja.kotovich@gmail.com>
2021-03-16 16:25:50 +0200

    Uzdevums-06 redakteju js

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,8 @@
 // Your JavaScript goes here.
+window.onload = function(){
+    var enterYourName = document.getElementById("full_name");
+    enterYourName.addEventListener("keyup", function(){
+      var greetings = document.getElementById("greeting");
+      greetings.innerHTML = ("Greetings," + enterYourName.value + "!");
+    });
+  }; 
